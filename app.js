1ca56116d6c1f1f22328ae1a9620114925129668
Imanda <asis03ktm@gmail.com>
2022-02-11 16:58:16 +0545

    Folder name changed

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ const app = express();              // call express and startm from here
 app.use(express.static('public'));      //To use static file like css insdie the public folder
 
 //view engine
-app.set('view engine', 'html');
+// app.set('view engine');
 
 // datebase connection
-const dburl = 'mongodb+srv://project:imanda123@cluster0.ogiuh.mongodb.net/node-auth';
-mongoose.connect(dburl,{usenewParser: true, useUnfiedTopology: true,useCreateIndex: true})
-.then((resukt) => app.listen(3000))
-.catch((error) => console.log(error));
+const dburl = ("mongodb://localhost/eSchool");
+mongoose.connect(dburl)
+app.listen(3000);
+// .then((resukt) => app.listen(3000))
+// .catch((error) => console.log(error));
 
 // routes
 app.get('/',(req,res) => res.render('login'));
 app.get('/',(req,res) => res.render('register'));
-app.use(authRoutes);
+app.use('/auth',authRoutes);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,10 @@ const authController = require('../controller/authController')
 
 const router = Router();
 
-router.get('./register',authController.register_get);
-router.post('./register',authController.register_post);
-router.get('./logIn',authController.logIn_get);
-router.post('./logIn',authController.logIn_post);
+router.get('/register',authController.register_get);
+router.post('/register',authController.register_post);
+router.get('/logIn',authController.logIn_get);
+router.post('/logIn',authController.logIn_post);
 
 
 
