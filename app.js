e3f9696062c0effb76493ec0e5e63098de640994
Imanda <asis03ktm@gmail.com>
2022-02-12 14:45:19 +0545

    cookies primer and hashing password

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');                 // import the express
 const mongoose = require('mongoose');               // import the mongoose
 const authRoutes = require('./routes/authRoutes')
 // const expressLayouts = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();              // call express and startm from here
 
 //middleware
 app.use(express.static('public'));      //To use static file like css insdie the public folder
+app.use(express.json());
+pp.use(cookieParser());
 
 //view engine
-app.use(express.json());
 app.set('view engine','ejs');
 
 // datebase connection
@@ -24,3 +26,22 @@ mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true})
 app.get('/',(req,res) => res.render('home'));
 app.get('/',(req,res) => res.render('smoothies'));
 app.use('/auth',authRoutes);
+
+//cookies
+app.get('./set-cookies',(req,res) => {
+
+    // res.setheader('Set Cookie', 'newUser = true')
+
+    res.cookie('newUser',false);
+    res.cookie('isEmployee',true, maxAge: 1000*60*60*24, httpOnly:true);
+
+    res.send('You got the cookies!');
+});
+
+app.get('./read-cookies',(req,res){
+
+const cookies = req.cookies;
+consolr.log(cookies);
+
+res.json(cookies)
+});
--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const { isEmail } = require('validator')
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -24,7 +25,11 @@ userSchema.post('save',function (doc, next){
 });
 
 //fire a function before doc saved to db
-
+userSchema.pre('save', async function (next){
+    const salt = await bcrypt.gensalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
+})
 
 const User = mongoose.model('user',userSchema);
 
