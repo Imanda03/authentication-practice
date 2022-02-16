578a2358489a6f410da3690939ac7bed0f2b957d
Imanda <asis03ktm@gmail.com>
2022-02-16 20:20:49 +0545

    signup part 2

--- controller/authController.js
+++ controller/authController.js
@@ -45,7 +45,7 @@ module.exports.signup_post = async (req, res) => {
         const user = await User.create({ email, password });
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(201).json(user);
+        res.status(201).json({ user: user._id });
     } catch (error) {
         let errors = handleErrors();
         res.status(400).json({ errors })
